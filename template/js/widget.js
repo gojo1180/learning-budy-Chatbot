8b545936fe60a9bddde182c3fe9b5e8988ea7c47
gojo1180 <tomiokagiyu8421@gmail.com>
2025-11-23 20:04:22 +0700

    perbaikan ui widget pop-up

--- template/js/widget.js
+++ template/js/widget.js
@@ -1,7 +1,7 @@
 (function() {
     // 1. Konfigurasi
     const API_BASE_URL = "http://localhost:8000"; // Sesuaikan jika backend di-deploy
-    const WIDGET_CSS_URL = API_BASE_URL + "/widget/widget.css"; // Asumsi CSS ada di backend
+    const WIDGET_CSS_URL = API_BASE_URL + "/widget/style/widget.css"; // Asumsi CSS ada di backend
     const VUE_CDN_URL = "https://unpkg.com/vue@3/dist/vue.global.js";
 
     // 2. Fungsi Pemuat (Loader)
@@ -34,10 +34,25 @@
             template: `
                 <div>
                     <!-- Tombol Chat -->
+                    <div>
+                    <!-- 
+                      TOMBOL CHAT FLOATING (Updated) 
+                      Sekarang menggunakan Icon SVG, bukan text 'Chat' 
+                    -->
                     <div class="chat-button" 
                          v-if="windowState !== 'fullscreen'" 
                          @click="togglePopup">
-                        Chat
+                        
+                        <!-- Ikon Close (X) jika popup terbuka -->
+                        <svg v-if="windowState === 'popup'" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+
+                        <!-- Ikon Chat Bubble jika tertutup -->
+                        <svg v-else viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12.375m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.159 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
+                        </svg>
+                    </div>
                     </div>
                     
                     <!-- Jendela Chat Utama -->
